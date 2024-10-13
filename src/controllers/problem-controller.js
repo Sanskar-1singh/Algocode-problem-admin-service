df088b6ec2280ae468adb22facc61919f58cfb74
Sanskar-1singh <sanskarsingh812@gmail.com>
2024-10-13 16:26:29 +0530

    added status codes using npm packages

--- src/controllers/problem-controller.js
+++ src/controllers/problem-controller.js
@@ -1,3 +1,5 @@
+const { StatusCodes }=require('http-status-codes');
+
 function pingptoblemcontroller(req,res){
     return res.json({
         message:"ping controller is up"
@@ -5,23 +7,33 @@ function pingptoblemcontroller(req,res){
 }
 
 function addProblem(req,res){
-
+  return res.Status(StatusCodes.NOT_IMPLEMENTED).json({
+    message:"NOT IMPLEMENTED"
+  });
 }
 
 function getProblem(req,res){
-
+    return res.Status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"NOT IMPLEMENTED"
+      });
 }
 
 function getProblems(req,res){
-
+    return res.Status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"NOT IMPLEMENTED"
+      });
 }
 
 function deleteProblem(req,res){
-
+    return res.Status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"NOT IMPLEMENTED"
+      });
 }
 
 function updateProblem(req,res){
-
+    return res.Status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"NOT IMPLEMENTED"
+      });
 }
 
 module.exports={
