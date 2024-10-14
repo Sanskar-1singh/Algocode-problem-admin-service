189bf7b4dce38ffe52ae3dd73815b97e9abc9771
Sanskar-1singh <sanskarsingh812@gmail.com>
2024-10-14 12:18:46 +0530

    succesfully setup database config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const { PORT }=require('./config/server.config');
 
 const apiRouter=require('./routes/index');
 const errorHandler = require('./utils/errorHandler');
+const connectToDB = require('./config/db.config');
 
 const app=express();
 
@@ -23,6 +24,8 @@ app.get('/ping',(req,res)=>{
 //last middleware if any error comes>>
 app.use(errorHandler);
 
-app.listen(PORT,()=>{
+app.listen(PORT, async ()=>{
     console.log(`Server started at PORT ${PORT}`);
+    await connectToDB();
+    console.log("successfully conncted to db");
 })
--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -19,3 +19,5 @@ function errorHandler(err,req,res,next){
 
 
 module.exports=errorHandler;
+
+
