5530a2330fdff5436c201d5ffddbc9d8c6db9913
Sanskar-1singh <sanskarsingh812@gmail.com>
2024-10-16 09:17:31 +0530

    added schema into model layer

---
+++ src/models/problem.models.js
@@ -0,0 +1,41 @@
+const mongoose=require('mongoose');
+
+const problemSchema=new mongoose.Schema({
+
+    title:{
+        type:String,
+        required:[true,'title cannot be empty']
+    },
+
+    description:{
+        type:String,
+        required:[true,'description cannot be empty']
+    },
+    difficulty:{
+            type:String,
+            enum:['easy','medium','hard'],
+            required:[true,'difficulty cannot be empty'],
+            default:'easy'
+    },
+    testcases:[
+        {
+            input:{
+                type:String,
+                required:true
+            },
+            output:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    editorial:{
+        type:String
+    }
+});
+
+
+//this is object using which we can execute our ORM queries>>
+const Problem=mongoose.model('Problem',problemSchema);
+
+module.exports=Problem;
